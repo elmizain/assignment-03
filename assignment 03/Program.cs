5a73a737196651898a961a5f2dfaa66cf6f03ad7
Mahmoud <mahmoudelmizayin@gmail.com>
2026-07-24 23:43:10 +0300

    question 2

--- assignment 03/Program.cs
+++ assignment 03/Program.cs
@@ -4,15 +4,24 @@
     {
         static void Main(string[] args)
         {
-            // Q1: String Immutability
-            
-            // =========================
-            string title = "clean code";
-            string upperTitle = title.ToUpper();
+            //// Q1: String Immutability
 
-            Console.WriteLine("Q1:");
-            Console.WriteLine("Original: " + title);
-            Console.WriteLine("Upper: " + upperTitle);
+            //// =========================
+            //string title = "clean code";
+            //string upperTitle = title.ToUpper();
+
+            //Console.WriteLine("Q1:");
+            //Console.WriteLine("Original: " + title);
+            //Console.WriteLine("Upper: " + upperTitle);
+
+            // Q2: String Interning
+            // =========================
+            string str1 = "Clean Code";
+            string str2 = "Clean Code";
+
+            Console.WriteLine("\nQ2:");
+            Console.WriteLine(Object.ReferenceEquals(str1, str2));
+
         }
     }
 }
