b04924a6fd49de6321daed2f0aaa30a0c065f073
Mahmoud <mahmoudelmizayin@gmail.com>
2026-07-24 23:34:22 +0300

    Add project files.

---
+++ assignment 03/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace assignment_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Q1: String Immutability
+            // =========================
+            string title = "clean code";
+            string upperTitle = title.ToUpper();
+
+            Console.WriteLine("Q1:");
+            Console.WriteLine("Original: " + title);
+            Console.WriteLine("Upper: " + upperTitle);
+        }
+    }
+}
