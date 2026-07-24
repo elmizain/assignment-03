e02df564555307c4f73393b1bef2d45af11784ab
Mahmoud <mahmoudelmizayin@gmail.com>
2026-07-24 23:50:31 +0300

    question 3

--- assignment 03/Program.cs
+++ assignment 03/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace assignment_03
+﻿using System.Text;
+
+namespace assignment_03
 {
     internal class Program
     {
@@ -14,14 +16,25 @@
             //Console.WriteLine("Original: " + title);
             //Console.WriteLine("Upper: " + upperTitle);
 
-            // Q2: String Interning
+            //// Q2: String Interning
+            //// =========================
+            //string str1 = "Clean Code";
+            //string str2 = "Clean Code";
+
+            //Console.WriteLine("\nQ2:");
+            //Console.WriteLine(Object.ReferenceEquals(str1, str2));
+
+            // Q3: StringBuilder Append
             // =========================
-            string str1 = "Clean Code";
-            string str2 = "Clean Code";
+            StringBuilder sb = new StringBuilder();
 
-            Console.WriteLine("\nQ2:");
-            Console.WriteLine(Object.ReferenceEquals(str1, str2));
+            sb.Append("Book List");
+            sb.Append(" - Updated");
 
+            Console.WriteLine("\nQ3:");
+            Console.WriteLine(sb);
+
+            //======================
         }
     }
 }
